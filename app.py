bde8cd48b24cacdfd334a0786bd5c81e861c0846
Subhanker Kumar <152485605+penguin-svg@users.noreply.github.com>
2025-12-27 18:42:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -201,4 +201,7 @@ if __name__ == '__main__':
     
 
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
